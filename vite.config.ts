d89c0cbda586f528da2ea071d88794659fd8c79c
fabioavf <amorelli.ff@gmail.com>
2025-05-28 15:11:20 -0300

    adjust vite settings

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio/", // Make sure this matches your deployment path
+  base: "/",
   build: {
     assetsDir: "assets",
     rollupOptions: {
